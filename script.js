dc84744818f68749087f8a225b582492472dd75f
forwot <176093262+forwot@users.noreply.github.com>
2026-03-16 10:16:24 +0800

    add: library constructor logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("HI");
+const myLibrary = [];
+
+function Book(title, author, pages) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+  // take params, create a book then store it in the array
+  let book = new Book(title, author, pages);
+  myLibrary.push(book);
+}
+
+function displayMyLibrary(arr){
+    for(let book of arr){
+        console.log(`ID: ${book.id}, Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}`)
+    }
+}
+
