5d2260e5175cad2ed68467bd8370b25245eca44d
forwot <176093262+forwot@users.noreply.github.com>
2026-03-16 10:55:37 +0800

    add: book adding UI

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, stat) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.stat = stat;
 }
 
-function addBookToLibrary(title, author, pages) {
+function addBookToLibrary(title, author, stat) {
   // take params, create a book then store it in the array
-  let book = new Book(title, author, pages);
+  let book = new Book(title, author, stat);
   myLibrary.push(book);
 }
 
-function displayMyLibrary(arr){
-    for(let book of arr){
-        console.log(`ID: ${book.id}, Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}`)
+function displayMyLibrary(){
+    for(let book of myLibrary){
+        console.log(`ID: ${book.id}, Title: ${book.title}, Author: ${book.author}, Status: ${book.stat}`)
     }
 }
+
+let inputTitle = document.querySelector('#title');
+let inputAuthor = document.querySelector('#author');
+let inputStat = document.querySelector('#stat');
+let addButton = document.querySelector('.add-button');
+
+addButton.addEventListener("click", () => {
+    let title = inputTitle.value;
+    let author = inputAuthor.value;
+    let stat = inputStat.value;
+
+    addBookToLibrary(title, author, stat);
+    displayMyLibrary();
+})
 
